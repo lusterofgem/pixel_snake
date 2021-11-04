aae01902d01b1584528125bd77850f82476aaf5b
lusterofgem <lusterofgem@gmail.com>
2021-11-04 19:57:58 +0800

    Add color change to BeginStartAnimation

--- lib/ui/animations/begin/begin_start_animation.dart
+++ lib/ui/animations/begin/begin_start_animation.dart
@@ -12,12 +12,21 @@ class BeginStartAnimation extends BaseAnimation {
   int stateChangingFrame = 9;
   // Changing to which game state when it is the frame to switching state.
   GameState? targetGameState = GameState.playing;
+
   // The start center position of the full screen animation
   Offset startCenter = Offset(50, 87.5);
+  // The end center position of the full screen animation
+  Offset endCenter = Offset(50, 50);
+
   // The start size of the to full screen animation
   Size startSize = Size(60, 15);
   // The end size of the to full screen animation
   Size endSize = Size(100, 100);
+
+  // The start color of the animation
+  Color startColor = Color(0xFF52EB85);
+  // The end color of the animation
+  Color endColor = Color(0xFF66FF99);
 
   /****************************************************************************************************
    * Draw this animation on the given canvas.
@@ -28,38 +37,105 @@ class BeginStartAnimation extends BaseAnimation {
   void drawOnCanvas(Canvas canvas, Size screenSize) {
     this.screenSize = screenSize;
 
-    // draw
-    final currentRect = getCurrentRect();
-    canvas.drawRect(
-      Rect.fromCenter(center: Offset(_toAbsoluteWidth(currentRect.left), _toAbsoluteHeight(currentRect.top)),
-                      width: _toAbsoluteWidth(currentRect.width),
-                      height: _toAbsoluteHeight(currentRect.height)),
-      Paint()
-        ..color = Color(0xFF66FF99),
-    );
+    // Draw animation
+    if(frameIndex < animationLength) {
+      final currentCenter = getCurrentCenter();
+      final currentSize = getCurrentSize();
+      final currentColor = getCurrentColor();
+      canvas.drawRect(
+        Rect.fromCenter(center: Offset(_toAbsoluteWidth(currentCenter.dx), _toAbsoluteHeight(currentCenter.dy)),
+                        width: _toAbsoluteWidth(currentSize.width),
+                        height: _toAbsoluteHeight(currentSize.height)),
+        Paint()
+          ..color = currentColor,
+      );
+    }
+    // Warning when the frame index is invalid but this function is called
+    else {
+      print("Warning: drawOnCanvas(Canvas, Size) called, but the frameIndex: ${frameIndex} is invalid.");
+    }
   }
 
   /****************************************************************************************************
-   * Calculate current animation square rect.
-   * The size range is from startSize to endSize.
+   * Calculate current Size.
+   * The range is from startCenter to endCenter.
    ****************************************************************************************************/
-  Rect getCurrentRect() {
-    // The size change amount in each frame of the animation
-    Offset eachFrameChangedSize = Offset(endSize.width - startSize.width, endSize.height - startSize.height) / stateChangingFrame.toDouble();
-    // Calculate the current size
-    Size currentSize = startSize + eachFrameChangedSize * frameIndex.toDouble();
+  Offset getCurrentCenter() {
+    Offset currentCenter = Offset(0, 0);
 
-    // The end center should at the center of the screen
-    final endCenter = Offset(50, 50);
-    // The center changed of each frame
-    Offset eachFrameCenterOffset = (endCenter - startCenter) / stateChangingFrame.toDouble();
-    // The current center point
-    Offset currentCenter = startCenter + eachFrameCenterOffset * frameIndex.toDouble();
+    if(frameIndex <= stateChangingFrame) {
+      currentCenter = startCenter;
 
-    print(currentCenter);
+      // The center changed of each frame
+      Offset eachFrameCenterOffset = (endCenter - startCenter) / stateChangingFrame.toDouble();
+      // The current center point
+      currentCenter += eachFrameCenterOffset * frameIndex.toDouble();
+    }
+    else if(frameIndex <= animationLength - 1) {
+      currentCenter = endCenter;
+    }
 
-//     return Rect.fromLTWH(0, 0, currentSize.width, currentSize.height);
-    return Rect.fromLTWH(currentCenter.dx, currentCenter.dy, currentSize.width, currentSize.height);
+    return currentCenter;
+  }
+
+  /****************************************************************************************************
+   * Calculate current Size.
+   * The range is from startSize to endSize.
+   ****************************************************************************************************/
+  Size getCurrentSize() {
+    Size currentSize = Size(0, 0);
+
+    if(frameIndex <= stateChangingFrame) {
+      currentSize = startSize;
+
+      // The size change amount in each frame of the animation
+      Offset eachFrameChangedSize = Offset(endSize.width - startSize.width, endSize.height - startSize.height) / stateChangingFrame.toDouble();
+      // Calculate the current size
+      currentSize += eachFrameChangedSize * frameIndex.toDouble();
+    }
+    else if(frameIndex <= animationLength - 1) {
+      currentSize = endSize;
+    }
+
+    return currentSize;
+  }
+
+  /****************************************************************************************************
+   * Calculate current color.
+   * The range is from startColor to endColor.
+   ****************************************************************************************************/
+  Color getCurrentColor() {
+    Color currentColor = Color(0x000000);
+
+    if(frameIndex <= stateChangingFrame) {
+      // The color red value change amount in each frame of the animation
+      double eachFrameChangedRed = (endColor.red - startColor.red) / stateChangingFrame.toDouble();
+      // The color green value change amount in each frame of the animation
+      double eachFrameChangedGreen = (endColor.green - startColor.green) / stateChangingFrame.toDouble();
+      // The color blue value change amount in each frame of the animation
+      double eachFrameChangedBlue = (endColor.blue - startColor.blue) / stateChangingFrame.toDouble();
+
+      currentColor = Color.fromARGB(
+        startColor.alpha,
+        startColor.red + (eachFrameChangedRed * frameIndex).round(),
+        startColor.green + (eachFrameChangedGreen * frameIndex).round(),
+        startColor.blue + (eachFrameChangedBlue * frameIndex).round(),
+      );
+    }
+    // Fade out
+    else if(frameIndex <= animationLength - 1) {
+      // The color alpha value change amount in each frame of the animation
+      double eachFrameChangedAlpha = (endColor.alpha - startColor.alpha) / stateChangingFrame.toDouble();
+
+      currentColor= Color.fromARGB(
+        endColor.alpha + (eachFrameChangedAlpha * frameIndex).round(),
+        endColor.red,
+        endColor.green,
+        endColor.blue,
+      );
+    }
+
+    return currentColor;
   }
 
   /****************************************************************************************************
