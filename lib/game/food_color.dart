891d526962be1748518e380c50c1911d7a4149b5
lusterofgem <lusterofgem@gmail.com>
2022-03-09 13:20:23 +0800

    Change colors to list

--- lib/game/food_color.dart
+++ lib/game/food_color.dart
@@ -1,9 +1,11 @@
 import 'dart:ui';
 
 class FoodColor {
-  static const Color food0 = Color(0xFFFF7FED);
-  static const Color food1 = Color(0xFFFF7FED);
-  static const Color food2 = Color(0xFFFF7FED);
-  static const Color food3 = Color(0xFFFF7FED);
-  static const Color food4 = Color(0xFFFF7FED);
+  static List<Color> colors = [
+    const Color(0xFFFF7FED),
+    const Color(0xFFFF7FED),
+    const Color(0xFFFF7FED),
+    const Color(0xFFFF7FED),
+    const Color(0xFFFF7FED),
+  ];
 }
